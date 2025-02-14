Fixed some layout issues, started implementing timeline tab and retrained the models without data leakage on validation set

indigenous_project/tainacan_collection/utils.py
@@ -5,7 +5,7 @@ from matplotlib.colors import LinearSegmentedColormap
 
 from PIL import Image
 import ast
-import requests
+import math
 
 import pandas as pd
 import numpy as np
@@ -49,9 +49,6 @@ def fig_update_layout(fig, df_len, x_range=(-norm_factor,norm_factor), y_range=(
         # Mouse default configuration (panning instead of zooming)
         dragmode='pan',
         hoverdistance = 6,
-
-        # # Animating graphs
-        # transition=dict(duration=500, easing='sin-in-out')
     )
 
     # Hide axes' labels and ticks
@@ -121,8 +118,149 @@ def empty_figure_legend(color_df, x_range=(-norm_factor,norm_factor), y_range=(-
 
     return fig
 
-# Create timeline figure
+# Utility function to create a downward half-circle arc between two rows for the timeline
+def make_turn_arc(x0, y0, side="right", radius=0.5, steps=20):
+    if side == 'right':
+        start_angle = math.pi / 2
+        end_angle = -math.pi / 2
+    else:
+        start_angle = math.pi / 2
+        end_angle = 3 * math.pi / 2
 
+    cx = x0
+    cy = y0 - 0.5
+    angles = np.linspace(start_angle, end_angle, steps)
+    arc_pts = []
+    for angle in angles:
+        arc_x = cx + radius * math.cos(angle)
+        arc_y = cy + radius * math.sin(angle)
+        arc_pts.append((arc_x, arc_y))
+
+    arc_pts[0] = (x0, y0)
+    arc_pts[-1] = (x0, y0 - 1)
+    return arc_pts
+
+# Create timeline figure with clickable markers for years 
+def timeline_figure(years):
+
+    # Computing zigzag coordinates
+    years = -np.sort(-years)
+    cols = 8
+    n_years = len(years)
+    n_rows = math.ceil(n_years/cols)
+    line_points = []
+    year_points = []
+    annotations = []
+    idx = 0
+    direction = -1
+    for row in range(n_rows):
+        row_years = min(cols, n_years-row*cols)
+        col_indices = range(row_years)
+        for c in col_indices:
+            if direction == 1:
+                x = c+0.2
+            else:
+                x = cols-1-c-0.2
+            y = -row
+            year = years[idx]
+            line_points.append((x, y))
+            year_points.append((x, y, year))
+
+            # Add annotation for the year under the marker
+            annotations.append(dict(
+                x=x,
+                y=y+0.2,
+                text=str(year),
+                showarrow=False,
+                font=dict(weight='bold'),
+                xanchor='center',
+                yanchor='top'
+            ))
+
+            idx += 1
+
+        if row < n_rows - 1:
+            end_x = (cols-1) if direction == 1 else 0
+            end_y = -row
+            side = 'right' if direction == 1 else 'left'
+            arc_pts = make_turn_arc(end_x, end_y, side, 0.5, 20)
+
+            line_points.extend(arc_pts[1:])
+
+        direction *= -1
+
+    # Separate out x, y from line_points
+    if line_points:
+        xs_line, ys_line = zip(*line_points)
+    else:
+        xs_line, ys_line = [], []
+
+    # Separate out x, y, text from year_points
+    if year_points:
+        xs_year, ys_year, txt_years = zip(*year_points)
+    else:
+        xs_year, ys_year, txt_years = [], [], []
+
+    # Plot the timeline as a continuous zigzagging line
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        x=xs_line,
+        y=ys_line,
+        mode='lines',
+        line=dict(color='#062a57', width=4),
+        hoverinfo='none',
+        name='timeline'
+    ))
+
+    fig.add_trace(
+        go.Scatter(
+            x=xs_year,
+            y=ys_year,
+            mode='markers',
+            marker=dict(size=30, color='#062a57', line=dict(width=6, color="#f2f2f2")),
+            text=[str(y) for y in txt_years],
+            hoverinfo='none',
+            name='years'
+        )
+    )
+
+
+    # Updating figure layout
+    fig.update_layout(
+        plot_bgcolor="#f2f2f2",
+        paper_bgcolor="#f2f2f2",
+        
+        font=dict(family="Roboto, sans-serif", size=16, color="black"),
+        
+        title_x=0.5,
+        margin=dict(l=0, r=0, t=0, b=0),
+        
+        showlegend=False,
+
+        xaxis_title=None,
+        yaxis_title=None,
+        coloraxis_showscale=False,
+
+        dragmode=None,
+        hoverdistance = 5,
+
+        annotations=annotations
+    )
+ 
+    fig.update_xaxes(
+        range=[-1, 8],
+        fixedrange=True,
+        visible=False
+    )
+
+    fig.update_yaxes(
+        range=[-9, 1],
+        fixedrange=True,
+        visible=False,
+        autorange='reversed'
+    )
+
+    return fig
 
 # Creating the map of Brazil and plotting markers on states
 def brazil_figure():

indigenous_project/tainacan_collection/visualization_tool.py
@@ -10,7 +10,7 @@ import pandas as pd
 from sklearn.cluster import DBSCAN
 import numpy as np
 
-from utils import brazil_states_dict, norm_factor, empty_figure, empty_figure_legend, brazil_figure, plot_with_markers, plot_with_images, collapse_cluster_points, generate_color_map, update_cluster_selection, get_dropdown_options
+from utils import brazil_states_dict, norm_factor, empty_figure, empty_figure_legend, timeline_figure, brazil_figure, plot_with_markers, plot_with_images, collapse_cluster_points, generate_color_map, update_cluster_selection, get_dropdown_options
 
 from sklearn.datasets import make_blobs
 
@@ -125,7 +125,7 @@ app.layout = html.Div([
                 html.Div(
                     className='tool-container',
                     children=[
-                        html.H1("Agrupamentos do Acervo", className='graph-title'),
+                        # html.H1("Agrupamentos do Acervo", className='graph-title'),
                         html.Div(
                             className='sidebar-graph',
                             children=[
@@ -213,16 +213,14 @@ app.layout = html.Div([
                                                             placeholder=f"{plot_df['comprimento'].min():.1f}",
                                                             value=round(plot_df['comprimento'].min(), 1),
                                                             step="any",
-                                                            style={'width': '80px'}
                                                         ),
-                                                        html.Label("Max:", style={'fontSize': '14px', 'marginLeft': '20px', 'marginRight': '5px'}),
+                                                        html.Label("Max", style={'fontSize': '14px', 'marginLeft': '20px', 'marginRight': '5px'}),
                                                         dcc.Input(
                                                             id='comprimento-max',
                                                             type='number',
                                                             placeholder=f"{plot_df['comprimento'].max():.1f}",
                                                             value=round(plot_df['comprimento'].max(), 1),
                                                             step="any",
-                                                            style={'width': '80px'}
                                                         ),
                                                     ],
                                                 ),
@@ -242,16 +240,14 @@ app.layout = html.Div([
                                                             placeholder=f"{plot_df['largura'].min():.1f}",
                                                             value=round(plot_df['largura'].min(), 1),
                                                             step="any",
-                                                            style={'width': '80px'}
                                                         ),
-                                                        html.Label("Max:", style={'fontSize': '14px', 'marginLeft': '20px', 'marginRight': '5px'}),
+                                                        html.Label("Max", style={'fontSize': '14px', 'marginLeft': '20px', 'marginRight': '5px'}),
                                                         dcc.Input(
                                                             id='largura-max',
                                                             type='number',
                                                             placeholder=f"{plot_df['largura'].max():.1f}",
                                                             value=round(plot_df['largura'].max(), 1),
                                                             step="any",
-                                                            style={'width': '80px'}
                                                         ),
                                                     ],
                                                 ),
@@ -271,16 +267,14 @@ app.layout = html.Div([
                                                             placeholder=f"{plot_df['altura'].min():.1f}",
                                                             value=round(plot_df['altura'].min(), 1),
                                                             step="any",
-                                                            style={'width': '80px'}
                                                         ),
-                                                        html.Label("Max:", style={'fontSize': '14px', 'marginLeft': '20px', 'marginRight': '5px'}),
+                                                        html.Label("Max", style={'fontSize': '14px', 'marginLeft': '20px', 'marginRight': '5px'}),
                                                         dcc.Input(
                                                             id='altura-max',
                                                             type='number',
                                                             placeholder=f"{plot_df['altura'].max():.1f}",
                                                             value=round(plot_df['altura'].max(), 1),
                                                             step="any",
-                                                            style={'width': '80px'}
                                                         ),
                                                     ],
                                                 ),
@@ -300,16 +294,14 @@ app.layout = html.Div([
                                                             placeholder=f"{plot_df['diametro'].min():.1f}",
                                                             value=round(plot_df['diametro'].min(), 1),
                                                             step="any",
-                                                            style={'width': '80px'}
                                                         ),
-                                                        html.Label("Max:", style={'fontSize': '14px', 'marginLeft': '20px', 'marginRight': '5px'}),
+                                                        html.Label("Max", style={'fontSize': '14px', 'marginLeft': '20px', 'marginRight': '5px'}),
                                                         dcc.Input(
                                                             id='diametro-max',
                                                             type='number',
                                                             placeholder=f"{plot_df['diametro'].max():.1f}",
                                                             value=round(plot_df['diametro'].max(), 1),
                                                             step="any",
-                                                            style={'width': '80px'}
                                                         ),
                                                     ],
                                                 ),
@@ -358,7 +350,7 @@ app.layout = html.Div([
                 html.Div(
                     className='timeline-container',
                     children=[
-                        dcc.Graph(id='timeline', config=config, figure=empty_figure()),
+                        dcc.Graph(id='timeline', config=config, figure=timeline_figure(ind_df['ano_de_aquisicao'].dropna().unique().astype(np.int16))),
                     ]
                 ),
             ],