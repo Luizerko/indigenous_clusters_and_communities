38e75fcc3c660de051a133e15331ba7fbb1644ec
lui <luisvz@gmail.com>
2025-04-12 18:09:57 -0300

    Implemented l2-norm for aggregating the attributions per token and cosine similarity for computing target for integrated gradients.

--- tainacan_collection/language_based/language_training_utils.py
+++ tainacan_collection/language_based/language_training_utils.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader, random_split, ConcatDataset
 from torchmetrics.classification import Accuracy, Precision, Recall
@@ -64,11 +65,13 @@ def get_dataloaders(dataset, batch_size=8, splits=None):
 
 # Captum compatible wrapper model for the vanilla BERTimbau. Notice that we need to use a scalar representation for the [CLS] token so we can evaluate how ir varies with the other tokens (integrated gradients as the name suggests). In this case, we decided to go for the norm of the embedding vector
 class VanillaWrappedModel(nn.Module):
-    def __init__(self, model, pad_token_id, device):
+    def __init__(self, model, pad_token_id, baseline_embedding, device, target_type='l2-norm'):
         super(VanillaWrappedModel, self).__init__()
         self.model = model.to(device)
         self.pad_token_id = pad_token_id
+        self.baseline_embedding = baseline_embedding.detach().to(device)
         self.device = device
+        self.target_type = target_type
         
     def forward(self, input_ids):
         # Moving (or guaranteeing) input_ids to proper device
@@ -80,7 +83,11 @@ class VanillaWrappedModel(nn.Module):
         # Extracting [CLS] token embedding and computing its L2 norm (for attributions target)
         outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
         cls_embedding = outputs.last_hidden_state[:, 0]
-        cls_scalar = cls_embedding.norm(p=2, dim=1)
+        
+        if self.target_type == 'l2-norm':
+            cls_scalar = cls_embedding.norm(p=2, dim=1)
+        elif self.target_type == 'cos-sim':
+            cls_scalar = F.cosine_similarity(cls_embedding, self.baseline_embedding, dim=1)
         
         return cls_scalar
     
@@ -97,10 +104,13 @@ def get_embeddings(model, tokenizer, input_ids, device):
     return cls_embeddings
 
 # Function to compute attributions via integrated gradients torwards the [CLS] token
-def get_attributions(lig, tokenizer, input_ids, baseline_input_ids, verbose=False, sample_num=0):
+def get_attributions(lig, tokenizer, input_ids, baseline_input_ids, attrib_aggreg_type='sum', verbose=False, sample_num=0):
     # Computing attributions and then summing over the embedding dimensions (because we compute attributions for every dimension of the tokens' embeddings, so we need some kind of aggregation to idedntify tokens individually)
     attributions, delta = lig.attribute(inputs=input_ids, baselines=baseline_input_ids, return_convergence_delta=True, n_steps=50)
-    attributions = attributions.sum(dim=-1)
+    if attrib_aggreg_type == 'sum':
+        attributions = attributions.sum(dim=-1)
+    elif attrib_aggreg_type == 'l2-norm':
+        attributions = attributions.norm(p=2, dim=-1)
     attributions = attributions.cpu().detach()
 
     # Normalizing to output easier to interpret and because we are more interested in the absolute importance of tokens rather then their signal
@@ -119,6 +129,10 @@ def get_attributions(lig, tokenizer, input_ids, baseline_input_ids, verbose=Fals
         new_tokens, new_scores = [], []
         counter = -1
         for i, (token, score) in enumerate(zip(tokens[1:-1], sample_attribution[1:-1])):
+            if token == '[PAD]':
+                new_tokens = new_tokens[:-1]
+                break
+
             if token[0] == '#':
                 new_tokens[counter] += token[2:]
                 new_scores[counter] += score
