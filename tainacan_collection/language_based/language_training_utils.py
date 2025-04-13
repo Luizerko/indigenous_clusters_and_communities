e4b3a4a1c438622b93222354a3d6b4895cc2a5f0
lui <luisvz@gmail.com>
2025-04-13 15:23:05 -0300

    Implemented pipeline for unsupervised learning fine-tuning of the model

--- tainacan_collection/language_based/language_training_utils.py
+++ tainacan_collection/language_based/language_training_utils.py
@@ -40,7 +40,7 @@ class TextDataset(Dataset):
         inputs = self.tokenizer(text, return_tensors='pt', padding='max_length', truncation=True, max_length=self.max_length)
         input_ids = inputs['input_ids'].squeeze(0)
         
-        # Creating labels if dataset is used for fine-tuning
+        # Creating labels if dataset is used for supervised fine-tuning
         label = []
         if self.clf_col is not None:
             label = self.df.iloc[idx][self.clf_col]
@@ -64,9 +64,9 @@ def get_dataloaders(dataset, batch_size=8, splits=None):
         return DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
 # Captum compatible wrapper model for the vanilla BERTimbau. Notice that we need to use a scalar representation for the [CLS] token so we can evaluate how ir varies with the other tokens (integrated gradients as the name suggests). In this case, we decided to go for the norm of the embedding vector
-class VanillaWrappedModel(nn.Module):
+class CaptumWrappedModel(nn.Module):
     def __init__(self, model, pad_token_id, baseline_embedding, device, target_type='l2-norm'):
-        super(VanillaWrappedModel, self).__init__()
+        super(CaptumWrappedModel, self).__init__()
         self.model = model.to(device)
         self.pad_token_id = pad_token_id
         self.baseline_embedding = baseline_embedding.detach().to(device)
@@ -92,13 +92,16 @@ class VanillaWrappedModel(nn.Module):
         return cls_scalar
     
 # Function to compute [CLS] token embeddings
-def get_embeddings(model, tokenizer, input_ids, device):
+def get_embeddings(model, tokenizer, input_ids, device, fine_tuned=False):
     # Computing attention mask
     attention_mask = (input_ids != tokenizer.pad_token_id).to(device).long()
     
     # And now the [CLS] token embeddings
     with torch.no_grad():
-        outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+        if not fine_tuned:
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+        if fine_tuned:
+            outputs = model.model(input_ids=input_ids, attention_mask=attention_mask)
         cls_embeddings = outputs.last_hidden_state[:, 0]
 
     return cls_embeddings
@@ -146,3 +149,122 @@ def get_attributions(lig, tokenizer, input_ids, baseline_input_ids, attrib_aggre
             print(f"{token:20} -> {score:.4f}")
 
     return attributions, delta
+
+# Class for the SimCSE models. The idea is to use fine-tune different models using the SimCSE approach with NT-Xent loss, which is an unsupervised contrastive loss, ideal for specializing the embedding model to our data without having to group it by categories
+class SimCSEModel(nn.Module):
+    def __init__(self, model, pad_token_id, device, dropout_prob=0.1):
+        super(SimCSEModel, self).__init__()
+        self.model = model.to(device)
+        self.dropout = nn.Dropout(dropout_prob)
+        
+        self.device = device
+        self.pad_token_id = pad_token_id
+        
+    def forward(self, input_ids):
+        # Moving (or guaranteeing) input_ids to proper device
+        input_ids = input_ids.to(self.device)
+
+        # Computing attention mask and moving it to device as well
+        attention_mask = (input_ids != self.pad_token_id).to(self.device).long()
+
+        # Extracting [CLS] token embedding and passing it through dropout to late compute NT-Xent loss
+        outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
+        cls_embedding = outputs.last_hidden_state[:, 0]
+        cls_embedding = self.dropout(cls_embedding)
+        
+        return cls_embedding
+    
+# Computing the NT-Xent loss. The embeddings in this case should have shape [2*batch_size, dim], because we want two different views (different masked dimensions) of each sample to use as positive pairs
+def nt_xent_loss(embeddings, device, temperature=0.05):
+    # Computing cosine similarity in a more optimized way
+    normalized_embeddings = F.normalize(embeddings, dim=1)
+    sim_matrix = torch.matmul(normalized_embeddings, normalized_embeddings.T).to(device)
+    
+    # Masking self-similarity
+    sim_matrix.fill_diagonal_(-1e9)
+
+    # Computing "logits" for cross-entropy
+    logits = sim_matrix/temperature
+
+    # Creating labels (positive pair mapping). Each embedding i matches with i+N if i<N, or i-N if i>=N
+    batch_size = embeddings.shape[0]//2
+    labels = torch.arange(2*batch_size).to(device)
+    positive_indices = (labels+batch_size)%(2*batch_size)
+    positive_indices = positive_indices.long()
+
+    # Finally computing loss
+    loss = F.cross_entropy(logits, positive_indices)
+    return loss
+
+# Training loop for the SimCSE
+def contrastive_training_loop(model, optimizer, train_dataloader, val_dataloader, device, epochs=10, temperature=0.05, patience=3):
+    # Varibales for saving the best model and early-stopping
+    best_val_loss = float('inf')
+    patience_counter = 0
+
+    # Saving indicies and embeddings for later usage
+    all_indices = []
+    all_embeddings = []
+
+    # Tracking loss
+    train_losses = []
+    val_losses = []
+
+    # Effective training loop
+    for epoch in tqdm(range(epochs)):
+        model.train()
+        train_loss = 0
+
+        for indices, input_ids, _ in train_dataloader:
+            # Saving indices
+            all_indices.append(indices)
+
+            # Moving appropriate tensors to device
+            input_ids = input_ids.to(device)
+            
+            # Duplicating batch for SimCSE
+            input_ids = torch.cat([input_ids, input_ids], dim=0)
+            
+            # Computing embeddings, saving them, and computing loss
+            embeddings = model(input_ids)
+            all_embeddings.append(embeddings.cpu().detach())
+
+            loss = nt_xent_loss(embeddings, temperature)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item()
+
+        avg_train_loss = train_loss/len(train_dataloader)
+        train_losses.append(avg_train_loss)
+
+        # Validation loss computation for early stopping and hyperparameter tunning
+        model.eval()
+        model.zero_grad()
+        val_loss = 0
+        with torch.no_grad():
+            for indices, input_ids, _ in val_dataloader:
+                # Repeating process for validation dataset
+                input_ids = input_ids.to(device)
+                input_ids = torch.cat([input_ids, input_ids], dim=0)
+                embeddings = model(input_ids)
+                val_loss += nt_xent_loss(embeddings, temperature).item()
+        
+        avg_val_loss = val_loss/len(val_dataloader)
+        val_losses.append(avg_val_loss)
+
+        print(f"Epoch {epoch+1}/{epochs} | Train Loss: {avg_train_loss:.4f} | Val Loss: {avg_val_loss:.4f}")
+
+        # Implementing early-stopping
+        if avg_val_loss < best_val_loss:
+            best_val_loss = avg_val_loss
+            patience_counter = 0
+            torch.save(model.state_dict(), "simcse_bertimbau.pt")
+        else:
+            patience_counter += 1
+            if patience_counter >= patience:
+                print("Early stopping!")
+                break
+
+    return all_indices, all_embeddings, train_losses, val_losses
