Using average between STS-B and In-Context STS-B for validation (early stopping and model saving), adding tests to albertina-based models and fixing a few bugs in the pipeline

tainacan_collection/language_based/language_training_utils.py
@@ -393,12 +393,13 @@ def usimcse_training_loop(model, tokenizer, optimizer, train_dataloader, val_dat
         print(f"Epoch {epoch+1}/{epochs} | Train Loss: {avg_train_loss:.4f} | Val Loss: {avg_val_loss:.4f} | STS-B: {stsb_track[-1]:.4f} | in-context STS-B: {in_context_stsb_track[-1]:.4f}")
 
         # Implementing early-stopping
-        if stsb_track[-1] > best_stsb:
+        if (stsb_track[-1]+in_context_stsb_track[-1])/2 > (best_stsb+best_in_context_stsb)/2:
             best_stsb = stsb_track[-1]
+            best_in_context_stsb = in_context_stsb_track[-1]
             patience_counter = 0
             torch.save({'epoch': epoch+1, 'model_state_dict': model.state_dict(), 'optimizer_state_dict': optimizer.state_dict()}, f'../data/models_weights/{model_name}.pth')
             print('New best model found!')
-        elif stsb_track[-1] > best_stsb*0.9:
+        elif (stsb_track[-1]+in_context_stsb_track[-1])/2 > 0.9*(best_stsb+best_in_context_stsb)/2:
             continue        
         else:
             patience_counter += 1
@@ -533,12 +534,13 @@ def infonce_training_loop(model, tokenizer, optimizer, train_dataloader, val_dat
         print(f"Epoch {epoch+1}/{epochs} | Train Loss: {avg_train_loss:.4f} | Val Loss: {avg_val_loss:.4f} | STS-B: {stsb_track[-1]:.4f} | in-context STS-B: {in_context_stsb_track[-1]:.4f}")
 
         # Implementing early-stopping
-        if stsb_track[-1] > best_stsb:
+        if (stsb_track[-1]+in_context_stsb_track[-1])/2 > (best_stsb+best_in_context_stsb)/2:
             best_stsb = stsb_track[-1]
+            best_in_context_stsb = in_context_stsb_track[-1]
             patience_counter = 0
             torch.save({'epoch': epoch + 1, 'model_state_dict': model.state_dict(), 'optimizer_state_dict': optimizer.state_dict()}, f'../data/models_weights/{model_name}.pth')
             print('New best model found!')
-        elif stsb_track[-1] > best_stsb*0.9:
+        elif (stsb_track[-1]+in_context_stsb_track[-1])/2 > 0.9*(best_stsb+best_in_context_stsb)/2:
             continue
         else:
             patience_counter += 1